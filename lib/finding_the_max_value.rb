b625ccb8abba688a0d9e4d538554992a0f5c5566
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-31 18:17:09 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,10 @@
 def find_max_value(array)
   max=0
   array.length.times { |index|
-  if array[index]>array[index+1]
-    max=array[index]
+  if array[index] <array[index+1]
+    max=array[index+1]
   else
-    max = array[index+1]
+    max = array[index]
   end
 }
 end
